184f4cabb2bf9a8dcfba95d9be2b14b9f7928bf3
Erum-Rehman <erumr0443@gmail.com>
2023-03-01 21:43:46 +0500

    sql setup

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'restaurant',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +78,22 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    },
+    'mydb': {   
+ 
+        'ENGINE': 'django.db.backends.mysql',   
+        'NAME': 'my_database',   
+        'USER': 'root',   
+        'PASSWORD': 'ErumSql123',   
+        'HOST': '127.0.0.1',   
+        'PORT': '3306',   
+        'OPTIONS': {   
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"   
+        } 
 }
+}
+# my.cnf
+# [client]
 
 
 # Password validation
